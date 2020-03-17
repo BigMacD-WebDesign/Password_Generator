256e98761c9056d1e95cb45e26ca6107917edb2e
Scott MacDonald <scottmacd88@yahoo.com>
2020-03-16 17:31:03 -0700

    Added the JavaScript and updated README

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lletters = "abcdefghijklmnopqrstuvwxyz";
+var scharacter = "#$%^&!"
+var nmbrs = "1234567890"
+var validCharacters = ""
+var validPassword = ""
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordlength = parseInt(prompt("How many characters?"))
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert("Please choose a number between 8 and 128.")
+  }
+  else {
+    var uppercaseletter = confirm("Do you want uppercase letters in your password?")
+    var lowercaseletter = confirm("Do you want lowercase letters in your password?")
+    var numbers = confirm("Do you want numbers in your password?")
+    var specialCharacter = confirm("Do you want special characters in your password?")
+    if (uppercaseletter) {
+      validCharacters += uletters
+    }
+    if (lowercaseletter) {
+      validCharacters += lletters
+    }
+    if (numbers) {
+      validCharacters += nmbrs
+    }
+    if (specialCharacter) {
+      validCharacters += scharacter
+    }
+    for (let i = 1; i <= passwordlength; i++) {
+      var index = Math.floor(Math.random() * validCharacters.length)
+      validPassword += validCharacters[index]
+    }
+    console.log(validPassword)
 
-  passwordText.value = password;
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = validPassword;
+  }
+
 
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Password_Generator
 Homework #3
+
+
+Coded the JavaScript.
+
+Gave the variables of what the password should generate randomly in lines 2-8
+
+Lines 11-36 set up the prompts and confirms in order to proceed and generate a password inside the text box.
